d7132d2fb711149439c5e8817d8f569722f10ff4
arseniy-grinev <arseniy.grinev@gmail.com>
2022-10-13 23:31:54 +0300

    12

---
+++ src/components/Profile/ProfileStatsList.jsx
@@ -0,0 +1,31 @@
+// import PropTypes from 'prop-types';
+// import { StatsTitle, StatsQuantity, ProfileStatsList } from './Profile.styled';
+
+// export default function ProfileStatsList({
+//   stats: { followers, views, likes },
+// }) {
+//   return (
+//     <ProfileStatsList class="stats">
+//       <li>
+//         <StatsTitle class="label">Followers</StatsTitle>
+//         <StatsQuantity class="quantity">{followers}</StatsQuantity>
+//       </li>
+//       <li>
+//         <StatsTitle class="label">Views</StatsTitle>
+//         <StatsQuantity class="quantity">{views}</StatsQuantity>
+//       </li>
+//       <li>
+//         <StatsTitle class="label">Likes</StatsTitle>
+//         <StatsQuantity class="quantity">{likes}</StatsQuantity>
+//       </li>
+//     </ProfileStatsList>
+//   );
+// }
+
+// Profile.propTypes = {
+//   stats: PropTypes.shape({
+//     followers: PropTypes.number.isRequired,
+//     views: PropTypes.number.isRequired,
+//     likes: PropTypes.number.isRequired,
+//   }),
+// };
