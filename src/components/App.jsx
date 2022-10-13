d0759ad22646f1b3201c330f42cb4a4c437c7857
arseniy-grinev <arseniy.grinev@gmail.com>
2022-10-13 18:43:21 +0300

    retask

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { AppWrap } from './App.styled';
+import { Profile } from './profile/Profile';
+import { Statistics } from './statistics/Statistics';
+import { Friends } from './friends/Friends';
+import { TransactionHistory } from './transaction/TransactionHistory';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <AppWrap>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Friends friends={friends} />
+      <TransactionHistory items={transactions} />
+    </AppWrap>
   );
 };
