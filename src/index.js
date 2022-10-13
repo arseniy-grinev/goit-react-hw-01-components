9d3f44795af14b436daffce0253d4c949611a6a6
arseniy-grinev <arseniy.grinev@gmail.com>
2022-10-13 18:06:16 +0300

    rr

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-01-components/">
+      <App />
+    </BrowserRouter>
+    {/* <App /> */}
   </React.StrictMode>
 );
